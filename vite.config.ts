af31ccd384427313a725945f129fdba03d19e1f1
Nacho <nachoballestrino02@gmail.com>
2024-11-14 12:05:12 -0300

    path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import path from 'path'
+import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
