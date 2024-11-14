44083948256a00d34b6559b569d95fcad6a246d5
Nacho <nachoballestrino02@gmail.com>
2024-11-14 12:34:14 -0300

    solve 7

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import * as path from 'path'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 export default defineConfig({
   plugins: [react()],
